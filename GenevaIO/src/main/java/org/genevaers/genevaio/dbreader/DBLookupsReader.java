1e31edfb75d29790a20256f37e15a4c82ffa9f15
hammyau <blueham60@gmail.com>
2024-12-10 15:44:48 +0800

    DB Lookup corrections
    Signed-off-by: hammyau <blueham60@gmail.com>

--- GenevaIO/src/main/java/org/genevaers/genevaio/dbreader/DBLookupsReader.java
+++ GenevaIO/src/main/java/org/genevaers/genevaio/dbreader/DBLookupsReader.java
@@ -129,8 +129,8 @@ public class DBLookupsReader extends DBReaderBase{
         lpKey.setKeyNumber(rs.getShort("KEYSEQNBR"));
         lpKey.setTargetLrId(rs.getInt("LOGRECID"));
         requiredLRs.add(rs.getInt("LOGRECID"));
-        lpKey.setSourceLrId(rs.getInt("LKUPSRCLR"));
-        requiredLRs.add(rs.getInt("LKUPSRCLR"));
+        lpKey.setSourceLrId(rs.getInt("STEPSRCLR"));
+        requiredLRs.add(rs.getInt("STEPSRCLR"));
         lpKey.setFieldId(rs.getInt("LRFIELDID"));
         lpKey.setDatatype(DataType.fromdbcode(getDefaultedString(rs.getString("VALUEFMTCD"), "NONE")));
         lpKey.setSigned(rs.getInt("SIGNED") == 0 ? false : true);
@@ -142,8 +142,13 @@ public class DBLookupsReader extends DBReaderBase{
         lpKey.setDateTimeFormat(DateCode.fromdbcode(getDefaultedString(rs.getString("FLDCONTENTCD"), "NONE")));
         lpKey.setJustification(JustifyId.fromdbcode(getDefaultedString(rs.getString("JUSTIFYCD"), "NONE")));
         lpKey.setMask(getDefaultedString(rs.getString("MASK"), ""));
-        lpKey.setValueLength(rs.getShort("VALUELEN"));
-        lpKey.setValue(getDefaultedString(rs.getString("VALUE"), ""));
+        String value = rs.getString("VALUE");
+        if(value != null && value.length() > 0) {
+            lpKey.setValueLength(value.length());
+            lpKey.setValue(value);
+        } else {
+            lpKey.setValueLength(0);
+        }
         lpKey.setSymbolicName(getDefaultedString(rs.getString("SYMBOLICNAME"), ""));
         Repository.addLookupPathKey(lpKey);
     }
