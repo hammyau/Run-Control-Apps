175e92e0d3f1b1a68f402dac70dbdd9aff0e0df8
gillianh <gillianh@au1.ibm.com>
2025-01-15 23:33:01 -0500

    add RCA build script for mf
    Signed-off-by: gillianh <gillianh@au1.ibm.com>

---
+++ mfbuild.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Copyright Contributors to the GenevaERS Project.
+#								SPDX-License-Identifier: Apache-2.0 (c) Copyright IBM Corporation
+#								2008
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+mvn clean
+mvn install -DskipTests $1
+export rev=`grep "<revision>" pom.xml | awk -F'<revision>||</revision>' '{print $2}'`;
+echo RCA release number $rev;
+
+cp RCApps/target/*-jar-with-dependencies.jar $GERS_RCA_JAR_DIR/rcapps-$rev.jar;                                       
+                                                                         
+cd $GERS_RCA_JAR_DIR;                                                    
+                                                                         
+touch rcapps-latest.jar;                                                 
+rm rcapps-latest.jar;                                                    
+ln -s rcapps-$rev.jar rcapps-latest.jar;
