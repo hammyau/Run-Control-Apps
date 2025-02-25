486cde011c6a996b615dbffafe256f00759952c7
hammyau <blueham60@gmail.com>
2025-02-25 13:43:27 +0800

    add LTFile package-info
    Signed-off-by: hammyau <blueham60@gmail.com>

--- GenevaIO/src/main/java/org/genevaers/genevaio/ltfile/package-info.java
+++ GenevaIO/src/main/java/org/genevaers/genevaio/ltfile/package-info.java
@@ -19,6 +19,11 @@
 
  /**
   * Classes to read and write logic table files.
+  * <p><b>What is a Logic Table?</b></p>
+  * <p>A logic table is a sequence of {@link org.genevaers.genevaio.ltfile.LTRecord} entires.</p>
+  * <p>Each table entry represents a Function Code. Which is an instruction to the Performance Engince.</p>
+  * <p>A loose but inaccurate equivalent of a functon code is a byte code in Java.</p>
+  * <p>A table describing the function codes can be found <a href="file:../../../../../../../ComponentGenerator/target/FCDescriptions.dot.svg">here</a>
   */ 
 package org.genevaers.genevaio.ltfile;
 
