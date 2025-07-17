f1b577f5acb93ad577d183cfdf28aec7a358b38b
AswiniV <aswini.vadlamudi@ibm.com>
2025-07-17 18:22:51 -0500

    Add ZeroSuppresion, FormatFilter tags to VDPXML
    Signed-off-by: AswiniV <aswini.vadlamudi@ibm.com>

--- GenevaIO/src/main/java/org/genevaers/genevaio/vdpxml/ViewOutputParser.java
+++ GenevaIO/src/main/java/org/genevaers/genevaio/vdpxml/ViewOutputParser.java
@@ -95,7 +95,7 @@ public class ViewOutputParser extends BaseParser {
 			case "MAXCHARSPERLINE":
 				vd.setOutputLineSizeMax((short) Integer.parseInt(text.trim()));
 				break;
-			case "ZEROSUPPRESSIND":
+			case "ZEROSUPPRESION":
 				vd.setZeroValueRecordSuppression(text.equals("0") ? false : true);
 				break;
 			case "EXTRACTMAXRECCNT":
@@ -125,7 +125,7 @@ public class ViewOutputParser extends BaseParser {
 			case "DELIMHEADERROWIND":
 				vd.setGenerateDelimitedHeader(text.equals("0") ? false : true);
 				break;
-			case "FORMATFILTLOGIC":
+			case "FILTER":
 				viewNode.setFormatFilterLogic(removeBRLineEndings(text));
 				break;
 			default:
